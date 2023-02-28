902976b526d89022b317e339589b4207fb679357
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-28 22:32:25 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+    }
+}
